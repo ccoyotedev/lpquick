e9f54103f889ce8fa625093c38917e2c7118a9dc
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2021-07-28 20:51:58 +0100

    Paused until dino token is added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Layout } from 'components/sections/layout';
 import { styled } from 'theme';
 import { Panel } from 'components/ui';
-
-const Grid = styled.section`
-  display: grid;
-  gap: 3.2rem;
-
-  @media ${({ theme }) => theme.mediaQueries.tablet} {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  @media ${({ theme }) => theme.mediaQueries.laptopL} {
-    grid-template-columns: repeat(3, 1fr);
-  }
-`
-
-const Card = styled.a`
-  display: block;
-  border: 1px solid ${({ theme }) => theme.colors.light2};
-  padding: 2.4rem;
-  color: ${({ theme }) => theme.colors.dark0};
-
-  :hover {
-    text-decoration: none;
-    border-color: ${({ theme }) => theme.colors.primaryAccent};
-  }
-
-  p {
-    margin: 0;
-  }
-`
+import { useMoralis } from 'react-moralis';
+import { useWeb3 } from 'context/Web3Context';
 
 const Home = () => {
+  const { Moralis, isWeb3Enabled, isAuthenticated, user } = useMoralis();
+  const { dispatch } = useWeb3();
+
+  const getBalances = async (address: string) => {
+    try {
+      const options = { chain: 'polygon', address: address, symbol: "DINO" }
+      const balances = await Moralis.Web3.getAllERC20(options);
+
+      console.log(options, balances)
+    } catch (err) {
+      dispatch({
+        type: "SET_ERROR",
+        error: err
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (isWeb3Enabled && isAuthenticated) {
+      const userAddress = user.attributes.accounts[0];
+      getBalances(userAddress);
+    }
+  }, [isWeb3Enabled, isAuthenticated])
+
   return (
     <Layout>
       <Panel>
-        <h1>Moralis Next.js</h1>
-        <p>Get started by editing <b>pages/index.tsx</b>.</p>
-        <Grid>
-          <Card href="https://nextjs.org/docs/basic-features/pages" target="_blank">
-            <h3>Next.js Documentation</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </Card>
-          <Card href="https://docs.moralis.io/" target="_blank">
-            <h3>Moralis Documentation</h3>
-            <p>Learn about Moralis' suite of features for easy web3 integration.</p>
-          </Card>
-          <Card href="https://styled-components.com/" target="_blank">
-            <h3>Styled components</h3>
-            <p>Learn about the CSS-in-JS tool that bridges the gap between components and styling.</p>
-          </Card>
-        </Grid>
+        
       </Panel>
     </Layout>
   )
